830e7bd68584ae3ebb28eee86e4e76489fecc0c2
rpatino12 <rpatino12@outlook.com>
2022-05-27 11:54:06 -0500

    Se crea la funcion que calcula el precio con descuento

--- avanzado/descuentos.js
+++ avanzado/descuentos.js
@@ -1,1 +1,19 @@
-alert("estoy conectado");
+// const precioOriginal = 120;
+// const porcentajeDescuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento)
+{
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento
+}
+
+
+
+// console.log({
+//     precioOriginal,
+//     porcentajeDescuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento
+// });
