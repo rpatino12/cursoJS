f19d0310c5fb7ae7d9473e628cafcf7d205265c4
rpatino12 <rpatino12@outlook.com>
2022-05-27 12:46:28 -0500

    Se integra el JavaScript con el html y se imprime el resultado dentro del mismo html

--- avanzado/descuentos.js
+++ avanzado/descuentos.js
@@ -1,6 +1,3 @@
-// const precioOriginal = 120;
-// const porcentajeDescuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento)
 {
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -9,11 +6,16 @@ function calcularPrecioConDescuento(precio, descuento)
     return precioConDescuento
 }
 
+function onClickButtonDiscount()
+{
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
 
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
 
-// console.log({
-//     precioOriginal,
-//     porcentajeDescuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento
-// });
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("resultPrice");
+    resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
