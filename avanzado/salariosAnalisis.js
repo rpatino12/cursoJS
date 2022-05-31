9a53d3b7a7ccf1439f0179deea6a29c809613eb6
rpatino12 <rpatino12@outlook.com>
2022-05-31 10:21:16 -0500

    Se agrega la funcion para el calculo de la mediana del top10 de salarios de colombia

--- avanzado/salariosAnalisis.js
+++ avanzado/salariosAnalisis.js
@@ -1,12 +1,4 @@
-const salariosCol = colombia.map(
-    function(personita)
-    {
-        return personita.salary;
-    }
-);
-
-const salariosColSorted = salariosCol.sort((a, b) => a - b);
-
+// Helpers
 function esPar(num) 
 {
     return (num % 2 === 0); // Si es par va a retornar true y si no, retorna false
@@ -24,7 +16,7 @@ function calcularMediaAritmetica(arr)
     return promedioArr;
 }
 
-
+// Calculadora de mediana
 function medianaSalarios(lista)
 {
     const mitad = parseInt(lista.length / 2);
@@ -44,4 +36,32 @@ function medianaSalarios(lista)
     }
 }
 
-console.log("La mediana de salarios en colombia es de: " + medianaSalarios(salariosColSorted));
+// Mediana general de salarios
+const salariosCol = colombia.map(
+    function(personita)
+    {
+        return personita.salary;
+    }
+);
+
+const salariosColSorted = salariosCol.sort((salaryA, salaryB) => salaryA - salaryB);
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+// Mediana del top 10%
+
+// Primer parametro del metodo splice, que sirve para indicar el indice a partir del cual quiero extraer los elementos del array original
+const spliceStart = salariosColSorted.length * 0.9;
+
+// Segundo parametro del metodo splice, que sirve para indicar la cantidad de elementos que quiero extraer del array original
+const spliceCount = salariosColSorted.length - spliceStart; 
+
+const salariosTop10Col = salariosColSorted.splice(spliceStart, spliceCount);
+
+const medianaTop10Col = medianaSalarios(salariosTop10Col);
+
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+});
